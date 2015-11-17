2b2b14a30c589756b862ac147b4bb85a1df96dba
bfrager <btfrager@gmail.com>
2015-11-17 12:20:48 -0800

    added CRUD functionality to controller

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,5 +1,7 @@
+//require todo model schema
 var Todo = require("../models/todo.js")
 
+//show all todos
 function index(req,res){
   Todo.find({}, function(err, todos){
     if (err) console.log(err)
@@ -7,7 +9,47 @@ function index(req,res){
   })
 }
 
+//add new todo
 function create(req,res){
   var todo = new Todo()
-  todo.
+  todo.name = req.body.name
+  todo.starred = req.body.starred
+  todo.checked = false
+  todo.save(function(err){
+    if (err) console.log(err)
+    res.json({success: true, message: 'Todo Created... yeah good luck with that'})
+  })
+}
+
+//show single todo details
+function show(req,res){
+  Todo.findById(req.params.id, function(err, todo){
+    if (err) console.log(err)
+    res.json(todo)
+  })
+}
+
+//update todo text
+function update(req,res){
+  Todo.findOneAndUpdate({_id: req.params.id}, {name: req.body.age}, function (err, todo){
+    if (err) console.log(err)
+    res.json(todo)
+  })
+}
+
+//delete todo
+function destroy(req,res){
+  Todo.remove({_id: req.params.id}, function (err){
+    if (err) console.log(err)
+    res.json(success: true, message: "Bye felicia!")
+  })
+}
+
+//export controller functions
+module.exports = {
+	index : index,
+	create: create,
+	show: show,
+	update: update,
+	destroy: destroy
 }
